552d0371d049dbf1af91ae4f7092dfcb34bc321b
squirrelRao <hqraop@163.com>
2022-02-08 16:20:23 +0800

    add Zuul and modify endponit url as *

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # saws_cluster_service_provider 服务客户端Client
 
-* SpringCloud、SpringBoot、Erueka
+* 框架：SpringCloud、SpringBoot
+* 服务注册：Erueka-client
 * 熔断和降级: Hystrix
+* 服务网关: Zuul
 
--- src/main/java/saws/SawsEurekaClientApplication.java
+++ src/main/java/saws/SawsEurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+//@EnableZuulProxy
 public class SawsEurekaClientApplication {
 
     @Bean
