756578f03b11b81631d9f1181141cdec8024a5ae
squirrelRao <hqraop@163.com>
2022-02-08 15:09:06 +0800

    add hystrix actuator

--- src/main/java/saws/SawsEurekaClientApplication.java
+++ src/main/java/saws/SawsEurekaClientApplication.java
@@ -3,14 +3,23 @@ package saws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
 public class SawsEurekaClientApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args){
         SpringApplication.run(SawsEurekaClientApplication.class);
